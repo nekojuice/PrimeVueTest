df21262e1175fa0e84184d9cd68a72dd30bb639e
Jose Wu <josewu@uictech.com.tw>
2024-05-31 14:42:05 +0800

    build: install PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// PrimeVue
+import PrimeVue from "primevue/config";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+// PrimeVue
+app.use(PrimeVue);
+
+app.mount("#app");
